23e84552491d36344f04553a73f300597f16a45c
yangchao <yangch_ts@qq.com>
2018-09-01 15:08:49 +0800

    beauty

--- tools/tips/publish.ps1
+++ tools/tips/publish.ps1
@@ -4,6 +4,6 @@ Copy-Item .\css .\dist\ -Recurse
 Copy-Item .\images .\dist\ -Recurse
 Copy-Item .\js .\dist\ -Recurse
 Copy-Item .\bower_components\dom-to-image\dist\dom-to-image.min.js .\dist\js\
-Copy-Item .\bower_components\weui\dist\style\weui.css .\dist\css\
+Copy-Item .\bower_components\weui\dist\style\weui.min.css .\dist\css\
 Copy-Item .\index.html .\dist\
 (Get-Content .\dist\index.html -Encoding UTF8).Replace('bower_components/dom-to-image/dist/', 'js/').Replace("bower_components/weui/dist/style/", "css/") | Set-Content .\dist\index.html -Encoding UTF8
